06c6f6aa7466e2349c7df048a8b2646cc420e112
Alf <147541233+A69NI@users.noreply.github.com>
2026-01-08 17:40:11 +0100

    Initiale Anlage

---
+++ Services/SensorService.cs
@@ -0,0 +1,47 @@
+﻿using NM.CORE.Domain.Entities;
+using NM.CORE.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NM.CORE.Application.Services
+{
+    public class SensorService
+    {
+        private readonly ISensorRepository _sensorRepository;
+
+        public SensorService(ISensorRepository sensorRepository)
+        {
+            _sensorRepository = sensorRepository;
+        }
+
+        // Implementation of SensorService class
+
+        public Task<IEnumerable<Sensor>> GetAllSensorsAsync()
+        {
+            return _sensorRepository.GetAllAsync();
+        }
+
+        public Task<Sensor?> GetSensorByIdAsync(int sensorId)
+        {
+            return _sensorRepository.GetByIdAsync(sensorId);
+        }
+
+        public Task AddSensorAsync(Sensor sensor)
+        {
+            return _sensorRepository.AddAsync(sensor);
+        }
+
+        public Task UpdateSensorAsync(Sensor sensor)
+        {
+            return _sensorRepository.UpdateAsync(sensor);
+        }
+
+        public Task<Sensor> DeleteSensorAsync(int sensorId)
+        {
+            return _sensorRepository.DeleteAsync(sensorId);
+        }
+    }
+}
---
+++ Services/TBL_AN_UNIProzess_MainTreeService.cs
@@ -0,0 +1,42 @@
+﻿using NM.CORE.Domain.Entities;
+using NM.CORE.Domain.Repositories;
+
+namespace NM.CORE.Application.Services
+{
+    public class TBL_AN_UNIProzess_MainTreeService
+    {
+        private readonly ITBL_AN_UNIProzesse_MainTreeRepository _tBL_AN_UNIProzesse_MainTreeRepository;
+
+        public TBL_AN_UNIProzess_MainTreeService(ITBL_AN_UNIProzesse_MainTreeRepository tBL_AN_UNIProzesse_MainTreeRepository)
+        {
+            _tBL_AN_UNIProzesse_MainTreeRepository = tBL_AN_UNIProzesse_MainTreeRepository;
+        }
+
+        // Implementation of SensorService class
+
+        public Task<IEnumerable<TBL_AN_UNIProzesse_MainTree>> GetAllAsync()
+        {
+            return _tBL_AN_UNIProzesse_MainTreeRepository.GetAllAsync();
+        }
+
+        public Task<TBL_AN_UNIProzesse_MainTree?> GetByIdAsync(int sensorId)
+        {
+            return _tBL_AN_UNIProzesse_MainTreeRepository.GetByIdAsync(sensorId);
+        }
+
+        public Task AddAsync(TBL_AN_UNIProzesse_MainTree tBL_AN_UNIProzesse_MainTree)
+        {
+            return _tBL_AN_UNIProzesse_MainTreeRepository.AddAsync(tBL_AN_UNIProzesse_MainTree);
+        }
+
+        public Task UpdateAsync(TBL_AN_UNIProzesse_MainTree tBL_AN_UNIProzesse_MainTree)
+        {
+            return _tBL_AN_UNIProzesse_MainTreeRepository.UpdateAsync(tBL_AN_UNIProzesse_MainTree);
+        }
+
+        public Task<TBL_AN_UNIProzesse_MainTree> DeleteAsync(int Id)
+        {
+            return _tBL_AN_UNIProzesse_MainTreeRepository.DeleteAsync(Id);
+        }
+    }
+}
